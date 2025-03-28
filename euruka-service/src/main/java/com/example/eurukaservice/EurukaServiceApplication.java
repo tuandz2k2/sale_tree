fcc5c8d52095ad5ef9e608402487bad9431340cb
Mình Tuấn Hoàng <minhtuan09012002@gmail.com>
2025-03-28 16:43:47 +0700

    start

---
+++ euruka-service/src/main/java/com/example/eurukaservice/EurukaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.eurukaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurukaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurukaServiceApplication.class, args);
+    }
+
+}
---
+++ euruka-service/src/test/java/com/example/eurukaservice/EurukaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurukaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurukaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
